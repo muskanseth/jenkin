56dc76c2ba91a9a0c333ee70ddc2599d3f0947c9
muskanseth <114662492+muskanseth@users.noreply.github.com>
2022-11-11 10:34:47 +0530

    Add files via upload
    commit changes

---
+++ jenkins/src/jenkins/jenkindemo.java
@@ -0,0 +1,11 @@
+package jenkins;
+
+public class jenkindemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi demo of testing jenkin");
+
+	}
+
+}
